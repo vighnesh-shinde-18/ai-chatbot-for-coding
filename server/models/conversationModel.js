242decee42a93f9a7ae6148f6338ef9d9d457a80
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-04-07 22:24:16 +0530

    Create conversationModel.js

---
+++ server/models/conversationModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const conversationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  featureType: String,
+  userInput: String,
+  aiOutput: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Conversation", conversationSchema);
