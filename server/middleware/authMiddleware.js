37c4f3aa2954de57b3c7a558c78e0b4047f2ceda
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-04-09 19:33:28 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,20 +2,23 @@ const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const authMiddleware = (req,res,next) =>{
-    const token = req.header("Authorization");
-
-    if( !token ){
-        return res.status(401).json({message : "Access Denied "});
+    
+    const authHeader = req.header('Authorization');
+    
+    if(!authHeader ||  !authHeader.startsWith("Bearer ")){
+        return next(); 
     }
 
+    const token = authHeader.split(" ")[1];
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
+        req.user = decoded; 
     }
     catch(error){
         res.status(400).json({message : "Invalid Token"})
-    }
+    } 
+    next();
 };
 
 module.exports = authMiddleware;
+ 
