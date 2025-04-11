3a43d3d811ecfc6e6d21a4705f8df114da1124a9
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-04-11 11:17:39 +0530

    Update aiControllers.js

--- server/controllers/aiControllers.js
+++ server/controllers/aiControllers.js
@@ -1,48 +1,37 @@
 const prompts = require('../utils/prompts');
-const callHuggingFaceAPI = require('../utils/huggingFaceClient');
-const Conversation = require('../models/conversationModel');
+const { generateContentFromPrompt } = require('../utils/googleGeminiApi'); // Importing the new function
+const saveConversation = require('../utils/saveConvertation');
 
 const processAIRequest = async (req, res) => {
-    try {
-        const { featureType, userInput, language } = req.body;
+  try {
+    const { featureType, userInput, language } = req.body;
 
-        if (!featureType || !userInput) {
-            return res.status(400).json({ error: "Missing required inputs." });
-        }
-
-        let prompt = prompts[featureType];
-
-        if (!prompt) {
-            return res.status(400).json({ error: "Invalid feature selected." });
-        }
-
-        let fullPrompt = prompt;
-        if (featureType === "convertCode" && language) {
-            fullPrompt += ` Convert following code into ${language} language , Code : `;
-        }
-
-        fullPrompt += `\n\n${userInput}`;
-
-        const result = await callHuggingFaceAPI(fullPrompt);
-
-        if (req.user) {
-            await Conversation.create({
-                userId: req.user.id,
-                featureType,
-                userInput,
-                aiOutput: result?.[0]?.generated_text || "No response",
-            });
-
-            res.status(200).json(result);
-        }
-    }
-    catch (e) {
-        res.status(500).json({ error: err.message });
+    if (!featureType || !userInput) {
+      return res.status(400).json({ error: "Missing required inputs." });
     }
 
+    let prompt = prompts[featureType];
+
+    if (!prompt) {
+      return res.status(400).json({ error: "Invalid feature selected." });
+    }
+
+    let fullPrompt = prompt;
+    if (featureType === "convertCode" && language) {
+      fullPrompt += `Convert following code into ${language} language , Code : `;
+    }
+    fullPrompt += userInput;
+
+    const response = await generateContentFromPrompt(fullPrompt);  
+    
+    console.log("response " , response.response.text())
+   saveConversation(req.user.id, featureType, userInput, response);
+
+    res.status(200).json(response);
+  }
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = { processAIRequest };
-
-
-
