a31bba3bce10e0d9e1b11c08162a88b65b000fb1
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-04-21 13:56:04 +0530

    Update NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const navigation = [
   { name: 'Debug', href: '#' },
+  { name: 'Review', href: '#' },
   { name: 'Generate', href: '#' },
   { name: 'Explain', href: '#' },
-  { name: 'Calendar', href: '#' },
+  { name: 'Convert', href: '#' },
+  { name: 'TestCases', href: '#' },
 ]
- 
 
 export default function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false)
+  const navigate = useNavigate()
+
+  const logOut = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include'
+      })
+      if (response.status === 200) {
+        navigate('/login')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <nav className="bg-gradient-to-r from-violet-200 to-pink-200">
@@ -24,7 +42,7 @@ export default function Navbar() {
             />
           </div>
 
-          <div className="hidden sm:flex space-x-4">
+          <div className="hidden md:flex space-x-4">
             {navigation.map((item) => (
               <a
                 key={item.name}
@@ -35,8 +53,8 @@ export default function Navbar() {
               </a>
             ))}
           </div>
- 
-          <div className="sm:hidden relative mr-0">
+
+          <div className="md:hidden relative mr-0">
             <button
               onClick={() => setDropdownOpen(!dropdownOpen)}
               className="inline-flex items-center rounded-md px-4 py-2 text-sm font-medium text-gray-700 shadow-md focus:outline-none"
@@ -70,9 +88,9 @@ export default function Navbar() {
             )}
           </div>
 
-          <div className=" items-center space-x-4  flex-column hidden sm:flex">
-            <span className="text-lg text-blue-500 font-medium cursor-pointer">Logout</span>
-            <img src="/src/assets/logo/log-out.png" alt="" className='size-8 text-sm text-white cursor-pointer' />
+          <div onClick={logOut} className="items-center space-x-4 flex-column hidden sm:flex cursor-pointer">
+            <span className="text-lg text-blue-500 font-medium">Logout</span>
+            <img src="/src/assets/logo/log-out.png" alt="Logout" className='size-8 text-sm text-white' />
           </div>
         </div>
       </div>
