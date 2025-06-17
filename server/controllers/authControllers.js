3bd47ef09ce90476253226a5c00008bb0531339d
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2025-06-17 13:22:10 +0530

    Update authControllers.js

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -76,10 +76,11 @@ exports.loginUser = async (req, res, next) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
 
     res.status(200).json({ 
       success: true,
